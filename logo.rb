bbfa20d96b4480827acfa7a3044949371822d4c3
sal202 <esteban.murcia0905@gmail.com>
2025-01-24 21:25:32 -0500

    prueva de git

--- logo.rb
+++ logo.rb
@@ -2,7 +2,7 @@ def imprimir_logo
  puts "       __        __   "
  puts "      /  \\      /  \\  "
  puts "     /    \\    /    \\ "
- puts "    /      \\  /      \\ "
+ puts "    /      \\  /        \\ "
  puts "   /        \\/        \\ "
  puts "  /          \\         \\ "
  puts " /            \\         \\ "
@@ -16,5 +16,6 @@ def imprimir_logo
   puts "  Reflejos"
 end
  
-
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
 imprimir_logo
